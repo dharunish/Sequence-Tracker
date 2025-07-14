c36d15c02c9d8811a45de4980e4d58022e31d419
Dharunish <dharyugi@gmail.com>
2025-07-13 21:04:28 -0400

    Initial Commit

---
+++ Sequence Tracker/Sequence_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sequence_TrackerApp.swift
+//  Sequence Tracker
+//
+//  Created by Dharunish Yugeswardeenoo on 7/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Sequence_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
